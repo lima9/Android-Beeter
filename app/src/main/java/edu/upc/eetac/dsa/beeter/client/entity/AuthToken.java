01434acb6bc07a490c4f88229849953cfc44f527
Max <max9@openmailbox.org>
2016-05-05 18:48:22 +0200

    1r commit

---
+++ app/src/main/java/edu/upc/eetac/dsa/beeter/client/entity/AuthToken.java
@@ -0,0 +1,36 @@
+package edu.upc.eetac.dsa.beeter.client.entity;
+
+import java.util.List;
+
+/**
+ * Created by Hermione on 05/05/2016.
+ */
+public class AuthToken {
+    private List<Link> links;
+    private String userid;
+    private String token;
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ app/src/main/java/edu/upc/eetac/dsa/beeter/client/entity/Sting.java
@@ -0,0 +1,7 @@
+package edu.upc.eetac.dsa.beeter.client.entity;
+
+/**
+ * Created by Hermione on 05/05/2016.
+ */
+public class Sting {
+}
